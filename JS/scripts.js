66c88c3d9407977fa9fdb109630c4a1a23ce5792
DaniloGoulart19 <danilolima718@gmail.com>
2024-06-28 15:44:36 -0300

    new file:   CSS/style.css 	new file:   JS/scripts.js 	modified:   index.html 	new file:   partials/testimonials.html

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+// JavaScript personalizado pode ser adicionado aqui
+// Por exemplo, inicializar o carrossel se necessário
+$(document).ready(function(){
+    $('#testimonialCarousel').carousel({
+        interval: 3000
+    });
+});
